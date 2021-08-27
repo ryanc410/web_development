2b0ca49c9f8f76e180c602fad60364b8c8259e1d
Ryan Cook <80371197+ryanc410@users.noreply.github.com>
2021-08-27 09:16:36 -0500

    08/27/2021
    Updated file to mysqli_connect

--- PHP/db_connect.php
+++ PHP/db_connect.php
@@ -1,23 +1,15 @@
-###############################
-#  D B  C O N N E C T
-###############################
-
 <?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
 
-$hostname = 'localhost';
-$user = 'username';
-$pass = 'password';
-$database = 'database_name';
-
-$db_connection = new PDO( "mysql:host=" . $hostname . ";dbname=" . $database, $user, $pass );
-
-$results = $db_connection->query( 'SELECT testimonial, author FROM recommendations WHERE 1 ORDER by rand() LIMIT 1' );
-
-foreach ( $results as $row ) {
-	echo '<p id="quote">' . $row['testimonial'] . '</p>';
-	echo '<p id="author">&ndash;' . $row['author'] . '</p>';
+// Create connection
+$conn= mysqli_connect($servername,$username,$password,$dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
-
-// Close the connection
-$db_connection = null;
+echo "Connected Successfully.";
+?>
 
